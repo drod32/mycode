eeb8fdef60e4b1e948468de47055c028d2b956be
drod32 <drod32@gmail.com>
2023-01-09 21:53:49 +0000

    learning about input function

---
+++ lab_input/inputChallenge01.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   CHALLENGE 01 - Solution"""
+
+def main():
+
+    user_input = input("finish the song: Old McDonald had a farm, and on that farm he had a...")
+
+    print("on that farm he had a", user_input, "E I E I O")
+
+
+main()
+
+
