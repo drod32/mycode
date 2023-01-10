c3bea8e712bbe8b54d058345a74867840d1dbe0d
drod32 <drod32@gmail.com>
2023-01-10 20:58:17 +0000

    Abot complex list

---
+++ advlist/animal-list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def main():
+
+    #create list of 5 animals
+    animalList = ["Fox", "Fly", "Ant", "Bee", "Cod"]
+        
+    print(f"these animals live on my farm {animalList}")
+
+main()
